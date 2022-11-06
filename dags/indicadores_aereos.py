d1f75558b6ad6a5614caab9d6bfed401dad98ccc
Ezequiel Lima <115598073+ezequielcdelima@users.noreply.github.com>
2022-11-06 18:20:58 -0300

    feat/atualizacao job spark 2

--- dags/indicadores_aereos.py
+++ dags/indicadores_aereos.py
@@ -4,6 +4,7 @@ from airflow.models import Variable
 from datetime import datetime
 import boto3
 
+
 aws_access_key_id = Variable.get('aws_access_key_id')
 aws_secret_access_key = Variable.get('aws_secret_access_key')
 
